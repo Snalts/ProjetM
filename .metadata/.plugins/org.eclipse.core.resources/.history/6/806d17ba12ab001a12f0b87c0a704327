094d4e870947232c3c26ca02e8e3bb80a2c6e81e
Snalts <alexandre.jodeau@orange.fr>
2020-06-10 15:44:05 +0200

    Modification Esprit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6/806d17ba12ab001a12f0b87c0a704327
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Statistique {
+
+	private int valeur;
+
+	private List bonus;
+
+	public void addBonus(Bonus bonus) {
+
+	}
+
+	public void rmBonus(Bonus bonus) {
+
+	}
+
+	public void setValeur() {
+
+	}
+
+}
--- Esprit.java
+++ Esprit.java
@@ -1,9 +1,29 @@
-/**Création de la class abstraite Esprit */
-public abstract class Esprit {
+public class Esprit{
     
-    protected Effet effet;
+    private EffetPourcentage effet;
 
-    protected String nom;
+    private String nom;
 
-    public abstract Effet activeEffet();
+    public enum Statut{FORCE,SANTE,VITESSE,CHANCE};
+
+    private Statut type;
+
+    public Esprit(EffetPourcentage effet, String nom,Statut type){
+        this.effet = effet;
+        this.nom = nom;
+        this.type = type;
+    }
+
+    public Effet activeEffet(){
+        return this.effet;
+    }
+ 
+    public String getNom(){
+        return this.nom;
+    }
+
+    public Statut getType(){
+        return this.type;
+    }
+
 }
